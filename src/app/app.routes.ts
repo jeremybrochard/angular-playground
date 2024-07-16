14a32fb6a93ede928f1940e98ec528d8c42c24d4
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-07-16 16:13:22 +0200

    feat: add flowers lazy component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,6 +15,11 @@ export const routes: Routes = [
     title: 'About',
   },
   {
+    path: 'flowers',
+    loadComponent: () => import('./pages/flowers.component'),
+    title: 'Flowers'
+  },
+  {
     path: '**',
     redirectTo: '',
   },
