6b5095244d8f64cdae227d24b044d7c9bdf55bd5
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-07-16 15:58:44 +0200

    refactor: use custom title-strategy to handle page titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home.component';
 import { AboutComponent } from './pages/about.component';
-import { setPageName } from './utils/set-page-name';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    title: setPageName('Home'),
+    title: 'Home',
     pathMatch: 'full',
   },
   {
     path: 'about',
     component: AboutComponent,
-    title: setPageName('About'),
+    title: 'About',
   },
   {
     path: '**',
---
+++ src/app/services/page-title-strategy.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
+import { setPageName } from '../utils/set-page-name';
+
+@Injectable({ providedIn: 'root' })
+export class PageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(setPageName(title));
+    }
+  }
+}
