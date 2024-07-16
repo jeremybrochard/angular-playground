41ef8ef152fae30765eb7067bc6a7358010e6721
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-07-16 15:38:18 +0200

    refactor: rename get-page-name function to set-page-name

--- standalone-components/src/app/utils/get-page-name.ts
+++
@@ -1,4 +0,0 @@
-
-export const getPageName = (name: string): `${string} | Standalone Components` => {
-  return `${name} | Standalone Components`;
-};
---
+++ standalone-components/src/app/utils/set-page-name.ts
@@ -0,0 +1,4 @@
+
+export const setPageName = (name: string): `${string} | Standalone Components` => {
+  return `${name} | Standalone Components`;
+};
