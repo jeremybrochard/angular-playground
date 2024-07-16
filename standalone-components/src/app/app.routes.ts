78c828ddfddbd9d0d2e3ff7e4486ac48b703c539
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-07-16 12:09:47 +0200

    refactor: set page titles using utils function

--- standalone-components/src/app/app.routes.ts
+++ standalone-components/src/app/app.routes.ts
@@ -1,18 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home.component';
 import { AboutComponent } from './pages/about.component';
+import { getPageName } from './utils/get-page-name';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    title: 'Home | Standalone Components',
+    title: getPageName('Home'),
     pathMatch: 'full',
   },
   {
     path: 'about',
     component: AboutComponent,
-    title: 'About | Standalone Components',
+    title: getPageName('About'),
   },
   {
     path: '**',
