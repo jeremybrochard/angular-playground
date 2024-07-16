301eb9e35647711698130ee299d49c5ed434b235
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-07-16 12:00:00 +0200

    feat: add basic routing mecanism with 2 simple pages

--- standalone-components/src/app/app.routes.ts
+++ standalone-components/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home.component';
+import { AboutComponent } from './pages/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home | Standalone Components',
+    pathMatch: 'full',
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'About | Standalone Components',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ standalone-components/src/app/pages/about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [],
+  template: `<p>This is about page!</p>`,
+  styles: ``,
+})
+export class AboutComponent {}
