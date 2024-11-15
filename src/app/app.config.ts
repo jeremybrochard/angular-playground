83d6e1d173365261e5cb8c581aa112f50c389e70
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-11-15 12:43:09 +0100

    feat: add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter, TitleStrategy } from '@angular/router';
 
 import { routes } from './app.routes';
 import { PageTitleStrategy } from './core/page-title-strategy';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    { provide: TitleStrategy, useClass: PageTitleStrategy },
+    { provide: TitleStrategy, useClass: PageTitleStrategy }, provideAnimationsAsync(),
   ],
 };
